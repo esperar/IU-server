88a7fc5186bfda34f65a7ef32fd3656dac18803c
aesperer <piyrw9754@gmail.com>
2023-01-22 15:16:28 +0900

    project setting

---
+++ src/main/java/com/IU/IuApplication.java
@@ -0,0 +1,13 @@
+package com.IU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuApplication.class, args);
+	}
+
+}

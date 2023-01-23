62aa17e89c74f25c6cd3e55b36fe02adf5c1d92f
aesperer <piyrw9754@gmail.com>
2023-01-23 13:26:13 +0900

    gitignore 적용하기

--- src/main/java/com/IU/IuApplication.java
+++ src/main/java/com/IU/IuApplication.java
@@ -2,8 +2,10 @@ package com.IU;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class IuApplication {
 
 	public static void main(String[] args) {

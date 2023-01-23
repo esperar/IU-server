266021d98c16822120a45680dce8436b5f7ffc9a
aesperer <piyrw9754@gmail.com>
2023-01-23 13:21:54 +0900

    :sparkles: security auth 추가
    추가

---
+++ src/main/java/com/IU/global/security/authentication/AuthDetailsService.java
@@ -0,0 +1,17 @@
+package com.IU.global.security.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new AuthDetails(email);
+    }
+}

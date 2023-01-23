6b305bfa7e0ba67c9af1c0ef2d02ffb47861eb3f
aesperer <piyrw9754@gmail.com>
2023-01-24 00:02:41 +0900

    :recycle: FeignConfig url 경로 수정
    경로 수정

--- src/main/java/com/IU/infrastructure/feign/config/FeignConfig.java
+++ src/main/java/com/IU/infrastructure/feign/config/FeignConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@EnableFeignClients(basePackages = "com.project.draw.infrastructure.feign")
+@EnableFeignClients(basePackages = "com.IU.infrastructure.feign")
 @Import(FeignClientErrorDecoder.class)
 @Configuration
 public class FeignConfig {
